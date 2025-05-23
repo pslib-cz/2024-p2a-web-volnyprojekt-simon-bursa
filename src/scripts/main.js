c14bc138874fdb7c2c41d2de83537e9d304ad34e
simon-bursa <simon.bursa.023@pslib.cz>
2025-05-23 11:23:43 +0200

    clarity

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,5 @@
 import Swiper from 'swiper/bundle';
+import Clarity from '@microsoft/clarity';
 
 // import styles bundle
 import 'swiper/css/bundle';
@@ -15,3 +16,7 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+const projectId = "rnzef1c6w2"
+
+Clarity.init(projectId);
